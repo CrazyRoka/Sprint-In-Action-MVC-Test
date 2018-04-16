bb0cf6534625afce7f0a1cb8b696aa315adabeaf
Rostislav Toch <RokaRostuk@gmail.com>
2018-04-16 23:18:27 +0300

    First Commit

---
+++ src/main/java/spittr/Spittle.java
@@ -0,0 +1,49 @@
+package spittr;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.util.Date;
+public class Spittle {
+    volatile static int counter = 1;
+    private final long id;
+    private final String message;
+    private final Date time;
+    private Double latitude;
+    private Double longitude;
+    public Spittle(String message, Date time) {
+        this(message, time, null, null);
+    }
+    public Spittle(
+            String message, Date time, Double longitude, Double latitude) {
+        this.id = counter++;
+        this.message = message;
+        this.time = time;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+    public long getId() {
+        return id;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public Date getTime() {
+        return time;
+    }
+    public Double getLongitude() {
+        return longitude;
+    }
+    public Double getLatitude() {
+        return latitude;
+    }
+    @Override
+    public boolean equals(Object that) {
+        if(!(that instanceof Spittle))return false;
+        Spittle spittle = (Spittle)that;
+        return new EqualsBuilder().append(time, ((Spittle) that).time).append(id, ((Spittle) that).id).isEquals();
+    }
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+}
---
+++ src/main/java/spittr/data/SpittleRepositoryImpl.java
@@ -0,0 +1,19 @@
+package spittr.data;
+
+import org.springframework.context.annotation.Configuration;
+import spittr.Spittle;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Configuration
+public class SpittleRepositoryImpl implements SpittleRepository {
+    public List<Spittle> findSpittles(long max, int count) {
+        List<Spittle> list = new ArrayList<Spittle>();
+        list.add(new Spittle("First!", new Date(), 33.0, 55.0));
+        list.add(new Spittle("Second!", new Date(), 54.0, 51.0));
+        list.add(new Spittle("Third!", new Date(), 16.3, 87.3));
+        return list;
+    }
+}
